1272335d215002968e77dc13826af407ad413d37
jan <bonawillis@naver.com>
2023-05-17 21:57:54 +0900

    Test: JPA Test

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class MemberRepositoryTest {
+
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional  // 트랜잭션어노테이션이 테스트에 있으면 테스트 끝난 후 롤백해버림.
+    @Rollback(false)        // 트랜잭션 롤백을 false로 바꿈
+    public void testMember() throws Exception {
+        // given
+        Member member = new Member();
+        member.setUserName("memberA");
+
+        // when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        // then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUserName()).isEqualTo(member.getUserName());
+        Assertions.assertThat(findMember).isEqualTo(member);
+        System.out.println("findMember == member : " + (findMember == member));
+
+    }
+}
